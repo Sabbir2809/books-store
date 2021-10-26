58529f3d612e4fa025df8f8333b367f2a671076d
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2021-10-26 22:52:12 +0600

    Our Blogs Section Added

--- js/script.js
+++ js/script.js
@@ -129,3 +129,26 @@ var swiper = new Swiper(".arrivals-slider", {
         },
       },
     });
+
+// Blogs
+var swiper = new Swiper(".blogs-slider", {
+  spaceBetween: 10,
+  grabCursor: true,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
