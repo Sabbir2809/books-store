ad31420e28d69be938cf257f0284421f6e0eb044
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2021-10-26 14:18:29 +0600

    Some Features Added

--- js/script.js
+++ js/script.js
@@ -1,21 +1,53 @@
 searchForm = document.querySelector(".search-form");
 document.querySelector("#search-btn").onclick = () => {
-    searchForm.classList.toggle("active");
-}
+  searchForm.classList.toggle("active");
+};
 window.onscroll = () => {
+  searchForm.classList.remove("active");
 
-    searchForm.classList.remove("active"); 
+  if (window.scrollY > 80) {
+    document.querySelector(".header .header-2").classList.add("active");
+  } else {
+    document.querySelector(".header .header-2").classList.remove("active");
+  }
+};
 
-    if(window.scrollY > 80){
-        document.querySelector(".header .header-2").classList.add("active");
-    }else{
-        document.querySelector(".header .header-2").classList.remove("active");
-    }
-}
+// Login Form
+let loginForm = document.querySelector(".login-form-container");
+
+document.querySelector("#login-btn").onclick = () => {
+  loginForm.classList.toggle("active");
+};
+
+document.querySelector("#close-login-btn").onclick = () => {
+  loginForm.classList.remove("active");
+};
+
 window.onload = () => {
-    if(window.scrollY > 80){
-        document.querySelector(".header .header-2").classList.add("active");
-    }else{
-        document.querySelector(".header .header-2").classList.remove("active");
-    }
-}
+  if (window.scrollY > 80) {
+    document.querySelector(".header .header-2").classList.add("active");
+  } else {
+    document.querySelector(".header .header-2").classList.remove("active");
+  }
+};
+
+// Swiper
+var swiper = new Swiper(".books-slider", {
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
