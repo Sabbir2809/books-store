93f7e52d2925fb79b73596c425f4914c1e7f54b2
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2021-10-26 22:21:20 +0600

    Reviews Section Added

--- js/script.js
+++ js/script.js
@@ -104,3 +104,28 @@ var swiper = new Swiper(".arrivals-slider", {
       },
     },
   });
+
+
+  // Reviews
+
+  var swiper = new Swiper(".reviews-slider", {
+    spaceBetween: 10,
+    grabCursor: true,
+      loop: true,
+      centeredSlides: true,
+      autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+      },
+    });
